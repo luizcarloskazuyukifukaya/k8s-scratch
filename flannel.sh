18ccc6f1dd0fdfee39681b12c95a52a43ec1ac21
Luiz Carlos Kazuyuki Fukaya <35419919+luizcarloskazuyukifukaya@users.noreply.github.com>
2019-05-26 21:11:21 +0900

    Update flannel.sh
    From Linux Academy

--- flannel.sh
+++ flannel.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# On ALL nodes
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+# On MASTER ONLY !!!!!!!
 kubectl apply -f \
   https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+
+#kubectl apply -f \
+#  https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
