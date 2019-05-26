dac9c19337360357b009163166e8688b911113c1
Luiz Carlos Kazuyuki Fukaya <35419919+luizcarloskazuyukifukaya@users.noreply.github.com>
2019-05-26 20:51:51 +0900

    Update 0.1-install-docker.sh
    Add comments for clarification of the commands

--- 0.1-install-docker.sh
+++ 0.1-install-docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Target Ubuntu 18.04 Bionic Beaver LTS
+
+# Add the Docker repository GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+# Add the Docker repository
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -10,8 +13,10 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
+# Install Docker-CE
 sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
 
+# Block updates
 sudo apt-mark hold docker-ce
 
 # Then verify with
